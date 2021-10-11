7124c277da981526d5da1fa174fbb4b1e74567f7
weiwei <weiwei@gosun.com>
2021-10-11 16:43:17 +0800

    config changed

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,12 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/noromol/',
-  plugins: [vue()]
+  plugins: [vue()],
+  server: {
+    port: 8080,
+    open: true,
+  },
+  build: {
+    sourcemap: true,
+  }
 })
